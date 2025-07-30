8e79ff0db1fb0b90f4e54999ed0a0b0edcdf9ccd
Shaun Dunmall <sdunmall@gmail.com>
2025-07-30 14:16:04 +0000

    In the future some data, such as patient demographic data,  with be pull

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,3 +8,10 @@ export interface ConsentFormCategory {
   category: string;
   forms: ConsentForm[];
 }
+
+export interface PatientData {
+    firstName: string;
+    lastName: string;
+    dob: string;
+    hospitalNumber: string;
+}
